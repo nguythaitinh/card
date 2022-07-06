72bc0d0acce5b6743970fdad02bbb77175fb87e9
kayaba <kayaba.fluctlight@gmail.com>
2022-07-06 10:12:50 +0700

    environment configure for Reanimated2

--- wings.config.js
+++ wings.config.js
@@ -18,8 +18,22 @@ const splitBundle = (configs) => {
 	return configs;
 };
 
+const setEnvironments = (configs, { webpack, wingsConfig }) => {
+	const { DefinePlugin } = webpack;
+	const env = wingsConfig.env();
+	const isProduction = wingsConfig.isProduction(env);
+
+	configs.plugins[0] = new DefinePlugin({
+		process: { env: {} },
+		__DEV__: !isProduction,
+		ENV: JSON.stringify(env),
+	});
+
+	return configs;
+};
+
 module.exports = {
-	webpackConfigs: [splitBundle],
+	webpackConfigs: [setEnvironments, splitBundle],
 	moduleAlias: () => {
 		return {
 			global: {
