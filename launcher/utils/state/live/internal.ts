3f93c46b4bb97eca540bb5b8a3e72916e58280d8
kayaba <kayaba.fluctlight@gmail.com>
2022-07-08 05:26:54 +0700

    watch/subscribe for Game invite from logged in account

--- launcher/utils/state/live/internal.ts
+++ launcher/utils/state/live/internal.ts
@@ -1,7 +1,9 @@
-import { proxy } from 'valtio';
+import { proxy, subscribe } from 'valtio';
+import { ObservableSubscription } from '@apollo/client';
 import { graphQlClient } from 'utils/graphql';
 import * as queries from 'utils/graphql/query';
 import * as subscriptions from 'utils/graphql/subscription';
+import { accountState } from 'utils/state/account';
 import { GameInvitation } from 'utils/types/graphql';
 
 export interface LiveState {
@@ -12,13 +14,25 @@ export const liveState = proxy<LiveState>({
 	gameInvites: [],
 });
 
-const invitationObservable = graphQlClient.subscribe({
-	query: subscriptions.gameInvitation,
-	variables: { opponent: 'Google_111926221361135077737' },
-});
+let invitationSub: ObservableSubscription;
+let lastAddress = accountState.profile?.address;
 
-invitationObservable.subscribe((payload) => {
-	liveState.gameInvites.push(payload.data.gameInvitation);
+subscribe(accountState, () => {
+	const nextAddress = accountState.profile?.address;
+
+	if (!!nextAddress && nextAddress !== lastAddress) {
+		invitationSub?.unsubscribe();
+		invitationSub = graphQlClient
+			.subscribe({
+				query: subscriptions.gameInvitation,
+				variables: { opponent: nextAddress },
+			})
+			.subscribe(({ data }) => {
+				liveState.gameInvites.push(data.gameInvitation);
+			});
+
+		lastAddress = nextAddress;
+	}
 });
 
 graphQlClient.query({ query: queries.gameInvitations }).then((response) => {
