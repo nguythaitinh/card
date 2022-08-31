f46a617f7a6c9f6d265c799ee4d553aa0159ec52
FME849 <1414439@gmail.com>
2022-08-31 18:26:02 +0700

    Refactor code in Heading section

--- launcher/screens/Marketplace/sections/Heading/index.tsx
+++ launcher/screens/Marketplace/sections/Heading/index.tsx
@@ -4,20 +4,20 @@ import { Text } from '@metacraft/ui';
 import { UnderRealmIcon } from 'components/icons/underRealm/MarkLogo';
 import resources from 'utils/resources';
 
-import { marketplaceSizes } from '../../shared';
+import { marketplaceSizes, marketplaceStyle } from '../../shared';
 
 export const HeadingSection: FC = () => {
 	return (
 		<View style={styles.container}>
 			<Image
-				style={styles.background}
+				style={marketplaceStyle.background}
 				source={resources.marketplace.headingBackground}
 			/>
 			<View style={styles.contentContainer}>
-				<UnderRealmIcon style={styles.icon} />
+				<UnderRealmIcon style={styles.icon} size={150} />
 				<View style={styles.titleContainer}>
 					<Text
-						style={styles.title}
+						style={marketplaceStyle.heading}
 						responsiveSizes={marketplaceSizes.responsiveHeadings}
 					>
 						Marketplace
@@ -35,31 +35,21 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-		paddingTop: 150,
+		paddingTop: 80,
 		paddingBottom: 30,
-	},
-	background: {
-		position: 'absolute',
-		top: 0,
-		bottom: 0,
-		left: 0,
-		right: 0,
+		marginBottom: 10,
 	},
 	contentContainer: {
 		position: 'relative',
 		alignItems: 'center',
 	},
 	icon: {
-		opacity: 0.8,
+		opacity: 0.3,
 	},
 	titleContainer: {
 		position: 'absolute',
 		top: 0,
 		bottom: 0,
 		justifyContent: 'center',
-	},
-	title: {
-		fontWeight: '600',
-		color: '#fff',
 	},
 });
