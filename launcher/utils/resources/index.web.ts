ceee3772eacb00e9e781a25e25daba73a2ec6d3a
FME849 <1414439@gmail.com>
2022-09-05 10:06:06 +0700

    Add resource to use in UnderRealmButton component

--- launcher/utils/resources/index.web.ts
+++ launcher/utils/resources/index.web.ts
@@ -2,5 +2,17 @@ export default {
 	marketplace: {
 		headingBackground: { uri: '/marketplace/heading-background.png' },
 		mainBackground: { uri: '/marketplace/main-background.png' },
+		underRealmInteractMaterial: {
+			normal: {
+				left: { uri: '/marketplace/under-realm-normal-left-edge.png' },
+				middle: { uri: '/marketplace/under-realm-normal-middle.png' },
+				right: { uri: '/marketplace/under-realm-normal-right-edge.png' },
+			},
+			hover: {
+				left: { uri: '/marketplace/under-realm-hover-left-edge.png' },
+				middle: { uri: '/marketplace/under-realm-hover-middle.png' },
+				right: { uri: '/marketplace/under-realm-hover-right-edge.png' },
+			},
+		},
 	},
 };
