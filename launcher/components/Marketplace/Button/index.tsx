eec645d83626e36fc66b92de9c8c3d652a8acc06
FME849 <1414439@gmail.com>
2022-09-13 17:13:45 +0700

    Refactor UnderRealmButton component

--- launcher/components/Marketplace/Button/index.tsx
+++ launcher/components/Marketplace/Button/index.tsx
@@ -36,13 +36,13 @@ export const UnderRealmButton: FC<Props> = ({
 	useHoveredStyle = useDefaultHoveredStyle,
 }) => {
 	const [layout, setLayout] = useState(idleLayout);
-	const source = resources.marketplace.underRealmInteractMaterial.normal;
+	const source = resources.marketplace.underRealmInteractMaterial;
 
 	const middle = {
 		position: 'absolute',
 		top: 0,
-		left: 20,
-		right: 20,
+		left: 5,
+		right: 5,
 		height: layout.height,
 	} as ImageStyle;
 
@@ -71,19 +71,38 @@ export const UnderRealmButton: FC<Props> = ({
 		>
 			{layout.width && (
 				<Fragment>
-					<Image style={middle} source={source.middle} resizeMode="repeat" />
-					<Image style={left} source={source.left} />
-					<Image style={right} source={source.right} />
+					<Fragment>
+						<Image
+							style={middle}
+							source={source.hover.middle}
+							resizeMode="repeat"
+						/>
+						<Image style={left} source={source.hover.left} />
+						<Image style={right} source={source.hover.right} />
+					</Fragment>
 				</Fragment>
 			)}
 			<View>
 				{children || <Text style={[styles.titleStyle, texStyle]}>{title}</Text>}
 			</View>
 			<Hoverable
-				style={{ ...middle, left: 0, right: 0 }}
+				style={{ ...middle, left: 0, right: 0, justifyContent: 'center' }}
 				animatedStyle={useHoveredStyle}
 			>
-				<AnimatedView />
+				<AnimatedView>
+					<Image
+						style={middle}
+						source={source.normal.middle}
+						resizeMode="repeat"
+					/>
+					<Image style={left} source={source.normal.left} />
+					<Image style={right} source={source.normal.right} />
+					<View>
+						{children || (
+							<Text style={[styles.titleStyle, texStyle]}>{title}</Text>
+						)}
+					</View>
+				</AnimatedView>
 			</Hoverable>
 		</TouchableOpacity>
 	);
