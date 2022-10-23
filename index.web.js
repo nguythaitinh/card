1270dd87227bdf52cd9bb54a96fa2df4562e2150
kayaba <kayaba.fluctlight@gmail.com>
2022-10-24 01:06:21 +0700

    configure aws module from entry

--- index.web.js
+++ index.web.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { configure } from './launcher/utils/lib';
 import App from './launcher';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+configure();
 
 root.render(<App />);
