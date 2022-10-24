574d3bc232567e6bafd01f1a4793bc27eb8c30c0
FME849 <1414439@gmail.com>
2022-10-24 12:01:22 +0700

    Replace ScrollView by ScrollLayout component

--- launcher/screens/Mint/Detail.tsx
+++ launcher/screens/Mint/Detail.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { AppState, appState } from '@metacraft/ui';
 import { useRoute } from '@react-navigation/native';
+import ScrollLayout from 'components/layouts/Scroll';
 import { packMap, PackStats } from 'screens/Mint/shared';
 import { useSnapshot } from 'utils/hook';
 
@@ -19,14 +20,14 @@ export const DetailScreen: FC = () => {
 	};
 
 	return (
-		<ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+		<ScrollLayout style={styles.container}>
 			<BannerSection dimensions={windowDimensions} />
 			<PackDetailSection
 				dimensions={windowDimensions}
 				pack={pack}
 				onPurchase={onPurchase}
 			/>
-		</ScrollView>
+		</ScrollLayout>
 	);
 };
 
@@ -35,6 +36,6 @@ export default DetailScreen;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#241414',
+		// backgroundColor: '#241414',
 	},
 });
