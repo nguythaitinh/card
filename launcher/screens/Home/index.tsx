48611e85d3e2315face4bdf12b50c939febb2eb5
kayaba <kayaba.fluctlight@gmail.com>
2022-10-25 05:08:44 +0700

    remove debuging text from Home, completely blank

--- launcher/screens/Home/index.tsx
+++ launcher/screens/Home/index.tsx
@@ -1,19 +1,13 @@
 import React, { FC } from 'react';
-import { StyleSheet } from 'react-native';
-import { Text } from '@metacraft/ui';
 import ScrollLayout from 'components/layouts/Scroll';
 import { iStyles } from 'utils/styles';
 
 export const HomeScreen: FC = () => {
 	return (
-		<ScrollLayout contentContainerStyle={iStyles.contentContainer}>
-			<Text>HomeScreen</Text>
-		</ScrollLayout>
+		<ScrollLayout
+			contentContainerStyle={iStyles.contentContainer}
+		></ScrollLayout>
 	);
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-	container: {},
-});
