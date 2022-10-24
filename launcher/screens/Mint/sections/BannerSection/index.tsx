5ffa117b5fc89503ec37a8c1d14d97944bc1ba52
kayaba <kayaba.fluctlight@gmail.com>
2022-10-25 05:06:19 +0700

    shadowed Intro and explains in Mint Banner

--- launcher/screens/Mint/sections/BannerSection/index.tsx
+++ launcher/screens/Mint/sections/BannerSection/index.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react';
 import { ImageBackground, ScaledSize, StyleSheet, View } from 'react-native';
+import { stormIcons } from '@metacraft/icons';
 import { ScaledSizes, Text } from '@metacraft/ui';
 import resources from 'utils/resources';
 import { iStyles } from 'utils/styles';
+
+const { Realm } = stormIcons;
 
 interface Props {
 	dimensions: ScaledSize;
@@ -11,12 +14,18 @@ interface Props {
 export const BannerSection: FC<Props> = () => {
 	return (
 		<ImageBackground
-			style={[iStyles.contentContainer, styles.container, { height: 724 }]}
+			style={[iStyles.contentContainer, styles.container, { height: 678 }]}
 			source={resources.mint.keyVisual}
 		>
 			<View style={styles.contentContainer}>
 				<Text style={styles.heading} responsiveSizes={responsiveHeadings}>
 					Get Genesis NFT Card
+				</Text>
+				<Text style={styles.intro}>
+					The very first and one of the largest Genesis card minting of{' '}
+					<Text style={styles.bold}>2,373 Genenis NFTs</Text> where you can get
+					unique chance to purchase and own those NFTs. Choose a Pack and get
+					one of the most valuable Genesis NFT ever!
 				</Text>
 			</View>
 		</ImageBackground>
@@ -25,7 +34,7 @@ export const BannerSection: FC<Props> = () => {
 
 export default BannerSection;
 
-const responsiveHeadings: ScaledSizes = [45, 45, 38, 30];
+const responsiveHeadings: ScaledSizes = [40, 38, 32, 24];
 
 const styles = StyleSheet.create({
 	container: {
@@ -40,8 +49,19 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 	},
 	heading: {
-		fontWeight: '600',
 		color: '#fff',
+		fontWeight: '600',
 		textAlign: 'center',
+		marginBottom: 28,
+		textShadow: '0 0 5px black',
+	},
+	intro: {
+		maxWidth: 800,
+		fontSize: 18,
+		textAlign: 'center',
+		textShadow: '0 0 10px black',
+	},
+	bold: {
+		fontWeight: '500',
 	},
 });
