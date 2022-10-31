612e455a33d6734f60ffad6457c8c335f858a3e2
FME849 <1414439@gmail.com>
2022-10-31 16:11:59 +0700

    Add general style in under realm home

---
+++ launcher/screens/Home/shared.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { ScaledSizes } from '@metacraft/ui';
+
+export const headingSize = [35] as ScaledSizes;
+
+export const sharedStyle = StyleSheet.create({
+	heading: {
+		fontWeight: '600',
+		color: '#fff',
+		marginBottom: 15,
+	},
+});
