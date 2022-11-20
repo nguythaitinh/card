7acde0e09031842599d2158962fa5d274fdd7ffa
Tinh Nguyen <tinh.nguyen@codelink.io>
2022-11-20 09:01:33 +0700

    Add Icon component

--- launcher/screens/Guide/Dashboard/Header/Icon.tsx
+++ launcher/screens/Guide/Dashboard/Header/Icon.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 import resources from '../../../../utils/resources';
 
@@ -7,32 +7,53 @@ import { ViewType } from '.';
 
 interface Props {
 	type: ViewType;
+	isActive?: boolean;
 }
 
-const Icon: FC<Props> = ({ type }: Props) => {
+const Icon: FC<Props> = ({ type, isActive = false }: Props) => {
 	let resource = null;
 	let label = null;
 
 	switch (type) {
 		case ViewType.Battlefield:
-			resource = resources.guide.battlefieldIconNormal;
+			resource = isActive
+				? resources.guide.battlefieldIconActive
+				: resources.guide.battlefieldIconNormal;
 			label = 'Battlefield';
 			break;
 		case ViewType.Play:
-			resource = require('./assets/play.png');
+			resource = isActive
+				? resources.guide.playIconActive
+				: resources.guide.playIconNormal;
 			label = 'Play';
 			break;
 		case ViewType.Card:
-			resource = require('./assets/card.png');
+			resource = isActive
+				? resources.guide.cardIconActive
+				: resources.guide.cardIconNormal;
 			label = 'Card';
 			break;
 	}
 
 	return (
 		<View>
-			<Image source={require('assets/images/launcher/launcher-icon.png')} />
+			<Image source={resource} style={styles.image} resizeMode="contain" />
+			<Text style={styles.label}>{label}</Text>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	image: {
+		width: 60,
+		height: 60,
+	},
+	label: {
+		fontSize: 9,
+		color: '#fff',
+		textAlign: 'center',
+		marginTop: 8,
+	},
+});
 
 export default Icon;
