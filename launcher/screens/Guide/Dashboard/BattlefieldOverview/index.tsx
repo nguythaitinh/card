8fa34f87a44ae6eabab2b756a8270d17a090af2f
Nguyen Thai Tinh <nguythaitinh@gmail.com>
2022-11-22 17:05:12 +0700

    Add onhover animation

--- launcher/screens/Guide/Dashboard/BattlefieldOverview/index.tsx
+++ launcher/screens/Guide/Dashboard/BattlefieldOverview/index.tsx
@@ -1,8 +1,21 @@
 import React, { FC, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import Animated, {
+	useAnimatedStyle,
+	withTiming,
+} from 'react-native-reanimated';
+import { Hoverable, Text } from '@metacraft/ui';
+
+import {
+	HoveredStyleFunc,
+	useDefaultHoveredStyle,
+} from '../../../../components/Marketplace/Button/shared';
 
 import Icon from './Icon';
+// import { HoveredStyleFunc, useDefaultHoveredStyle } from './shared';
 import Title from './Title';
+
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 export enum ViewType {
 	Hand,
@@ -60,12 +73,26 @@ const BattlefieldOverview: FC = () => {
 		viewTypeIcons[0],
 	);
 
+	const useHoveredStyle: HoveredStyleFunc = (isHovered) =>
+		useAnimatedStyle(() => ({
+			opacity: withTiming(isHovered.value ? 1 : 0.7, { duration: 250 }),
+		}));
+
 	return (
 		<View style={styles.container}>
 			<Title />
 			<View style={styles.iconsRow}>
 				{viewTypeIcons.map((viewTypeIcon) => (
-					<Icon key={viewTypeIcon.type} type={viewTypeIcon.type} />
+					<TouchableOpacity activeOpacity={0.7} key={viewTypeIcon.type}>
+						<Hoverable
+							style={{ left: 0, right: 0, justifyContent: 'center' }}
+							animatedStyle={useHoveredStyle}
+						>
+							<AnimatedView>
+								<Icon key={viewTypeIcon.type} type={viewTypeIcon.type} />
+							</AnimatedView>
+						</Hoverable>
+					</TouchableOpacity>
 				))}
 			</View>
 		</View>
